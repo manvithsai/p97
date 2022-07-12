a36742de9c766abe1a9df2247d7a603a13a49833
manvithsai <90837315+manvithsai@users.noreply.github.com>
2022-07-12 11:14:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+window.navigator("unicef").innerHTML=payment.html;
+window.navigator("go fund me").innerHTML=payment.html;
+window.navigator("99pledge").innerHTML=payment.html;
+<script src="main.js"></script>
